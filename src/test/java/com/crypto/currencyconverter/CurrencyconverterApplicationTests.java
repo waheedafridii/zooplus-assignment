f481bf07026d642a72eef8857733e72f17705bc6
waheedafridi <waheedafridii>
2022-01-28 18:31:35 +0400

    Initialize Spring Boot Application

---
+++ src/test/java/com/crypto/currencyconverter/CurrencyconverterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crypto.currencyconverter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CurrencyconverterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
