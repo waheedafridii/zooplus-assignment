06395487a2bcd0306d20ab9f70ce443f4f9b6999
waheedafridi <waheedafridii>
2022-01-30 17:50:45 +0400

    Add static webPage for integrating FrontEnd using thymeleaf

---
+++ src/main/java/com/crypto/currencyconverter/controller/WebController.java
@@ -0,0 +1,15 @@
+package com.crypto.currencyconverter.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/")
+public class WebController {
+
+    @GetMapping()
+    public String index(){
+        return "home";
+    }
+}
